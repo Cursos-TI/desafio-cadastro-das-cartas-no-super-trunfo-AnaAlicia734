29c34ac85b4d77b7b9118573b8225fcc7aefa1f4
AnaAlicia734 <familiaoliveira0906@gmail.com>
2025-03-20 22:58:13 +0000

    Crei duas cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,44 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    // Primeira Carta
+    int Carta = 1;
+    char estado1[50] = "Pará";
+    char codigo1[50] = "A01";
+    char cidade1[50] = "Belém";
+    int populacao1 = 1398531;
+    float Area1 = 1059.458;
+    float PIB1 = 30.8;
+    int Numero_de_pontos_turisticos1 = 50;
+
+    printf("Carta: %d\n", Carta);
+    printf("Estado: %s\n", estado1);
+    printf("Código: %s\n", codigo1);
+    printf("Cidade: %s\n", cidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f mk²\n", Area1);
+    printf("PIB: R$ %.2f Bilhões\n", PIB1);
+    printf("Número de pontos turísticos: %d\n", Numero_de_pontos_turisticos1);
+
+    // Segunda Carta
+    Carta = 2; 
+    char estado2[50] = "Ceará"; 
+    char codigo2[50] = "B02";
+    char cidade2[50] = "Fortaleza";
+    int populacao2 = 2574412; 
+    float Area2 = 312.353;
+    float PIB2 = 73.43;
+    int Numero_de_pontos_turisticos2 = 60;
+
+    printf("Carta: %d\n", Carta);
+    printf("Estado: %s\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Cidade: %s\n", cidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f mk²\n", Area2);
+    printf("PIB: R$ %.2f Bilhões\n", PIB2);
+    printf("Número de pontos turísticos: %d\n", Numero_de_pontos_turisticos2);
 
     return 0;
 }
+// Coloquei estado2, codigo2, cidade2, populacao2, Area2, PIB2 e Numero_de_pontos_turisticos2 como variáveis locais, pois são específicas da segunda carta. Pra não misturar com as variáveis da primeira carta.
